7cb1b394e1b159f6e6c89d44e644e67e160a089a
MarwenKing15 <marweng.touati@gmail.com>
2022-02-25 18:12:13 +0100

    changesss

--- Server.js
+++ Server.js
@@ -20,12 +20,12 @@ app.use("/api/hotel", hotelRouter);
 app.use("/api/hostel", hostelRouter);
 app.use("/api/restaurant", restaurantRouter);
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static("client/build"));
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// 	});
-// }
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const port = process.env.PORT || 5000;
 

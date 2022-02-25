8c3be5ea9594c835dece3c57cf7b4b20b1029784
MarwenKing15 <marweng.touati@gmail.com>
2022-02-25 21:27:54 +0100

    last

--- Server.js
+++ Server.js
@@ -20,22 +20,22 @@ app.use("/api/hotel", hotelRouter);
 app.use("/api/hostel", hostelRouter);
 app.use("/api/restaurant", restaurantRouter);
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static("client/build"));
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// 	});
-// }
-
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client", "build")));
-
+	app.use(express.static("client/build"));
 	app.get("*", (req, res) => {
-		const index = path.join(__dirname, "client", "build", "index.html");
-		// res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-		res.sendFile(index);
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
 }
+
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "client", "build")));
+
+// 	app.get("*", (req, res) => {
+// 		const index = path.join(__dirname, "client", "build", "index.html");
+// 		// res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// 		res.sendFile(index);
+// 	});
+// }
 
 const port = process.env.PORT || 5000;
 

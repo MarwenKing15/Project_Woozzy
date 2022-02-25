51c18e76dcd3695a801a2955b6ddf97ddc518c19
MarwenKing15 <marweng.touati@gmail.com>
2022-02-25 18:16:20 +0100

    chan

--- Server.js
+++ Server.js
@@ -20,10 +20,18 @@ app.use("/api/hotel", hotelRouter);
 app.use("/api/hostel", hostelRouter);
 app.use("/api/restaurant", restaurantRouter);
 
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static("client/build"));
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// 	});
+// }
+
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+	app.use(express.static(path.join(__dirname, "client", "build")));
+
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 	});
 }
 
